ff34e525ff030fb7bc0253424bd39bb3233925df
IKaoo <noecalmes@gmail.com>
2025-07-31 17:50:53 +0200

    remove lien table

--- API_Parc1/src/models/index.js
+++ API_Parc1/src/models/index.js
@@ -2,16 +2,7 @@ const Dinosaure = require('./Dinosaure');
 const Incident = require('./Incident');
 const Gardien = require('./Gardien');
 
-// Define associations
-Incident.belongsTo(Gardien, {
-  foreignKey: 'assignedKeeper',
-  as: 'keeper'
-});
-
-Gardien.hasMany(Incident, {
-  foreignKey: 'assignedKeeper',
-  as: 'incidents'
-});
+// Associations supprimées : aucun lien entre Incident et Gardien
 
 module.exports = {
   Dinosaure,
